106a32242bf57bf45c9485deef4ce13fbb03cfed
dewinurovilia <fauzy2327@gmail.com>
2026-05-30 07:42:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -425,3 +425,23 @@ item.classList.add(
 console.log(
 'Portal Operator SD Ready'
 );
+<script>
+function updateWaktu() {
+
+    const now = new Date();
+
+    document.getElementById('tanggal').innerHTML =
+        now.toLocaleDateString('id-ID',{
+            weekday:'long',
+            day:'numeric',
+            month:'long',
+            year:'numeric'
+        });
+
+    document.getElementById('jam').innerHTML =
+        now.toLocaleTimeString('id-ID');
+}
+
+updateWaktu();
+setInterval(updateWaktu,1000);
+</script>
